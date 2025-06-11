1a3c66b23a92b78ada18f31e38e6ff0fa0c7866e
Eddy Arriaga <eddyearriaga@gmail.com>
2025-06-11 13:32:44 -0500

    ActividadSumativa

---
+++ src/main/java/com/centroaprendizaje/ActividadSumativa.java
@@ -0,0 +1,13 @@
+package com.centroaprendizaje;
+
+import java.time.LocalTime;
+
+public abstract class ActividadSumativa {
+    protected String titulo;
+    protected LocalTime fechaDeEntrega;
+    protected float puntajeMaximo;
+    protected String contenido;
+    protected float calificacion;
+    protected String id;
+    
+}
