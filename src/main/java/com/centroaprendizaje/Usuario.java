ceb9738ff811973347d70052192a0f0c9dec6136
alan-am <alan.aguilardev@gmail.com>
2025-06-11 13:06:49 -0500

    Create base project

---
+++ src/main/java/com/centroaprendizaje/Usuario.java
@@ -0,0 +1,23 @@
+package com.centroaprendizaje;
+
+import java.util.ArrayList;
+
+public class Usuario {
+    //atributos
+    protected String usuario;
+    protected String contraseña;
+    protected String nombre;
+    protected String apellido;
+    protected ArrayList<Incidente> incidentes;
+
+
+    //metodos
+    protected boolean logIng(){
+        return true;
+    }
+
+     protected boolean logOut(){
+        return true;
+    }
+
+}
